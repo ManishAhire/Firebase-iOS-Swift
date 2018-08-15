b8611303caaf24d24be3593695cc3dba0ed6b6f0
manishahire <ahiremanishb@gmail.com>
2018-08-15 11:04:59 +0530

    Initial Commit

---
+++ Firebase Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Firebase Demo
+//
+//  Created by Munna on 15/08/18.
+//  Copyright © 2018 S2-SYSTEM. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Firebase DemoTests/Firebase_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Firebase_DemoTests.swift
+//  Firebase DemoTests
+//
+//  Created by Munna on 15/08/18.
+//  Copyright © 2018 S2-SYSTEM. All rights reserved.
+//
+
+import XCTest
+@testable import Firebase_Demo
+
+class Firebase_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
